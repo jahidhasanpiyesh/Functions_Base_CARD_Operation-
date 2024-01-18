b988a08f72acd2602d75ce587ba9cac48ba3a4c3
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2024-01-18 20:12:22 +0600

    Use to Placeholder and show help text .

--- cardapp/forms.py
+++ cardapp/forms.py
@@ -3,10 +3,10 @@ from .models import User
 class formsdata(forms.ModelForm):
     class Meta:
         model=User
-        fields = ('name','email','password')
+        fields = ('id','name','email','password')
         widgets = {
-            'name':forms.TextInput(attrs={'class':'form-control'}),
-            'email':forms.EmailInput(attrs={'class':'form-control'}),
-            'password':forms.PasswordInput(render_value=True, attrs={'class':'form-control'})
+            'name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Please Enter Your Valid Name'}),
+            'email':forms.EmailInput(attrs={'class':'form-control','placeholder':'Please Enter Your Valid Email Address'}),
+            'password':forms.PasswordInput(render_value=True, attrs={'class':'form-control','placeholder':'Please Enter Strong Password'})
             
         }
